453d4a20f970b45639dc5cbe2cef7dc786160e6b
Joseph Renner <jrenn3@gmail.com>
2023-07-19 16:52:17 -0500

    add moment js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@
 
 //The days array contains a list of the boat days and crew for the day
 window.days = [
-    {date: 6/5/2023,
+    {date: "6/5/2023",
     slot: "Eve",
+    event: "",
     crew: {
         person1: { name: "Jerry" , guestOf: ""},
         person2: { name: "Joe" , guestOf: ""},
@@ -33,6 +34,304 @@ window.days = [
         // person10: new person ("" , ""),
         // person11: new person ("" , ""),
         // person12: new person ("" , "")
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    event: "",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/24/2023,
+    slot: "Eve",
+    event: "Fireworks",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/2/2023,
+    slot: "Eve",
+    event: "",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/5/2023,
+    slot: "Eve",
+    event: "Fireworks",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/16/2023,
+    slot: "Day",
+    event: "",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/22/2023,
+    slot: "Eve",
+    event: "Fireworks",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/25/2023,
+    slot: "Eve",
+    event: "",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 7/29/2023,
+    slot: "Eve",
+    event: "Chicago Scene Boat Party",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 8/10/2023,
+    slot: "Eve",
+    event: "",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
+        }
+    },
+    {date: 6/15/2023,
+    slot: "Eve",
+    crew: {
+        person1: { name: "Jerry" , guestOf: ""},
+        person2: { name: "Joe" , guestOf: ""},
+        person3: { name: "Sean" , guestOf: ""},
+        person4: { name: "Ian" , guestOf: ""},
+        person5: { name: "Marsh" , guestOf: ""},
+        person6: { name: "Johnny" , guestOf: ""},
+        person7: { name: "Kirsten" , guestOf: "Jerry"},
+        person8: { name: "Marissa" , guestOf: "Joe"},
+        person9: { name: "" , guestOf: ""},
+        person10: { name: "" , guestOf: ""},
+        person11: { name: "" , guestOf: ""},
+        person12: { name: "" , guestOf: ""},
         }
     },
     {date: 6/15/2023,
@@ -83,52 +382,52 @@ function renderDay(day) {
             <hr>
             <div class = "crew">
                 <div class = "person">
-                    <p class = "name">${day.person1.name}</p>
-                    <p class = "guestOf">${day.person1.guestOf}</p>
+                    <p class = "name">${day.crew.person1.name}</p>
+                    <p class = "guestOf">${day.crew.person1.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person2.name}</p>
-                    <p class = "guestOf">${day.person2.guestOf}</p>
+                    <p class = "name">${day.crew.person2.name}</p>
+                    <p class = "guestOf">${day.crew.person2.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person3.name}</p>
-                    <p class = "guestOf">${day.person3.guestOf}</p>
+                    <p class = "name">${day.crew.person3.name}</p>
+                    <p class = "guestOf">${day.crew.person3.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person4.name}</p>
-                    <p class = "guestOf">${day.person4.guestOf}</p>
+                    <p class = "name">${day.crew.person4.name}</p>
+                    <p class = "guestOf">${day.crew.person4.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person5.name}</p>
-                    <p class = "guestOf">${day.person5.guestOf}</p>
+                    <p class = "name">${day.crew.person5.name}</p>
+                    <p class = "guestOf">${day.crew.person5.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person6.name}</p>
-                    <p class = "guestOf">${day.person6.guestOf}</p>
+                    <p class = "name">${day.crew.person6.name}</p>
+                    <p class = "guestOf">${day.crew.person6.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person7.name}</p>
-                    <p class = "guestOf">${day.person7.guestOf}</p>
+                    <p class = "name">${day.crew.person7.name}</p>
+                    <p class = "guestOf">${day.crew.person7.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person8.name}</p>
-                    <p class = "guestOf">${day.person8.guestOf}</p> 
+                    <p class = "name">${day.crew.person8.name}</p>
+                    <p class = "guestOf">${day.crew.person8.guestOf}</p> 
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person9.name}</p>
-                    <p class = "guestOf">${day.person9.guestOf}</p>
+                    <p class = "name">${day.crew.person9.name}</p>
+                    <p class = "guestOf">${day.crew.person9.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person10.name}</p>
-                    <p class = "guestOf">${day.person10.guestOf}</p>
+                    <p class = "name">${day.crew.person10.name}</p>
+                    <p class = "guestOf">${day.crew.person10.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person11.name}</p>
-                    <p class = "guestOf">${day.person11.guestOf}</p>
+                    <p class = "name">${day.crew.person11.name}</p>
+                    <p class = "guestOf">${day.crew.person11.guestOf}</p>
                 </div>
                 <div class = "person">
-                    <p class = "name">${day.person12.name}</p>
-                    <p class = "guestOf">${day.person12.guestOf}</p>
+                    <p class = "name">${day.crew.person12.name}</p>
+                    <p class = "guestOf">${day.crew.person12.guestOf}</p>
                 </div>
             </div>
         </div>
