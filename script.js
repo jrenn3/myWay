7e968d760bf77e432af7c9d1ae3bfce3d71143c2
Joseph Renner <jrenn3@gmail.com>
2023-10-03 20:48:15 -0500

    clean-up and trying to parse JSON file data

--- script.js
+++ script.js
@@ -1,132 +1,81 @@
-//The days array contains a list of the boat days and crew for the day
-window.days = [
-    {date: "10/2/2023",
-    slot: "Eve",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry", guestOf: "", phone: 7343652707},
-        person2: { name: "Joe", guestOf: "", phone: 8104239965},
-        person3: { name: "Sean", guestOf: "", phone: 4402428580},
-        person4: { name: "Ian", guestOf: "", phone: 2489824359},
-        person5: { name: "Marsh", guestOf: "", phone: 2487673509},
-        person6: { name: "Johnny", guestOf: "", phone: 2488606056},
-        person7: { name: "Kirsten", guestOf: "Jerry", phone: 6193180488},
-        person8: { name: "Marissa", guestOf: "Joe", phone: 6195073910},
-        person9: { name: null, guestOf: "", phone: ""},
-        person10: { name: null, guestOf: "", phone: ""},
-        person11: { name: null, guestOf: "", phone: ""},
-        person12: { name: "", guestOf: "", phone: ""},
-        } 
-    },
-    {date: "10/7/2023",
-    slot: "Eve",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
+function fetchData() {
+    return fetch('days.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            return data; // This is the JavaScript object parsed from the JSON file
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            throw error; // You can handle the error as needed
+        });
+}
+
+
+
+function startEdit(element) { //NEED TO REVIEW
+    var cell = element.parentNode;
+    var oldValue = element.innerText;
+    element.contentEditable = true;
+    element.focus();
+
+    element.addEventListener("blur", function () {
+        var newValue = element.innerText;
+        element.contentEditable = false;
+        if (oldValue !== newValue) {
+            // Update the cell content and perform any necessary data update here
+            oldValue = newValue;
         }
-    },
-    {date: "10/14/2023",
-    slot: "Day",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
-        }
-    },
-    {date: "10/15/2023",
-    slot: "Day",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
-        }
-    },
-    {date: "10/16/2023",
-    slot: "Eve",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
-        }
-    },
-    {date: "10/17/2023",
-    slot: "Eve",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
-        }
-    },
-    {date: "10/18/2023",
-    slot: "Eve",
-    event: "Sailing",
-    crew: {
-        person1: { name: "Jerry" , guestOf: ""},
-        person2: { name: "Joe" , guestOf: ""},
-        person3: { name: "Sean" , guestOf: ""},
-        person4: { name: "Ian" , guestOf: ""},
-        person5: { name: "Marsh" , guestOf: ""},
-        person6: { name: "Johnny" , guestOf: ""},
-        person7: { name: "Kirsten" , guestOf: "Jerry"},
-        person8: { name: "Marissa" , guestOf: "Joe"},
-        person9: { name: null , guestOf: ""},
-        person10: { name: null , guestOf: ""},
-        person11: { name: null , guestOf: ""},
-        person12: { name: null , guestOf: ""},
-        }
-    }
-]
+    });
+}
+
+function addName() {
+    var nameInput = document.getElementById("nameInput-10/2/2023");
+    var guestOfInput = document.getElementById("guestOfInput-10/2/2023");
+    var phoneInput = document.getElementById("phoneInput-10/2/2023");
+    var name = nameInput.value;
+    var guestOf = guestOfInput.value;
+    var phone = phoneInput.value;
+
+    
+    var rowData = { name: name, guestOf: guestOf, phone: phone };
+    
+    //****LEFT OFF HERE-FIND A WAY TO UPDATE SOME BACKGROUND DATA FOR A NEW TEAM MEMBER
+    var data = {};
+    data = fetchData();
+    data.push(rowData);
+    console.log(data);
+
+    nameInput.value = "";
+    guestOfInput.value = "";
+    phoneInput.value = "";
+}
+
+function removeName(button) {
+    var row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+}
+
+function render() {
+    return fetch('days.json')
+        .then(response => response.json())
+        .then(data => {
+            // Iterate through the data and render each day
+            data.forEach(day => {
+                const dayHtml = renderDay(day);
+                // Append the rendered HTML to the container element
+                const container = document.getElementById('expeditions'); 
+                container.innerHTML += dayHtml;
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
 
 //arrow fuction for loop to count the crew for one day
 const crewCount = (obj) => {
@@ -139,6 +88,7 @@ const crewCount = (obj) => {
     return count;
 };
 
+//----Render Functions----//
 //render a single day
 function renderDay(day) {
     return `
@@ -209,86 +159,8 @@ function renderDay(day) {
     `;
 }
 
-function startEdit(element) { //NEED TO REVIEW
-    var cell = element.parentNode;
-    var oldValue = element.innerText;
-    element.contentEditable = true;
-    element.focus();
-
-    element.addEventListener("blur", function () {
-        var newValue = element.innerText;
-        element.contentEditable = false;
-        if (oldValue !== newValue) {
-            // Update the cell content and perform any necessary data update here
-            oldValue = newValue;
-        }
-    });
-}
-
-function addName() {
-    var nameInput = document.getElementById("nameInput-10/2/2023");
-    var guestOfInput = document.getElementById("guestOfInput-10/2/2023");
-    var phoneInput = document.getElementById("phoneInput-10/2/2023");
-    var name = nameInput.value;
-    var guestOf = guestOfInput.value;
-    var phone = phoneInput.value;
-
-    //FIND A WAY TO UPDATE SOME BACKGROUND DATA FOR A NEW TEAM MEMBER
-    var rowData = { name: name, guestOf: guestOf, phone: phone };
-    days.push(rowData);
-
-    // ADD THE FOLLOWING BACK w/ more validation
-    // if (name.trim() === "") {
-    //     alert("Please enter a valid name.");
-    //     return;
-    // }
-
-    // ADD TO THE TABLE
-    // var table = document.getElementById("crew-10/2/2023");
-    // var newRow = table.insertRow(0);
-    // var cell1 = newRow.insertCell(1);
-    // var cell2 = newRow.insertCell(2);
-    
-    // cell1.innerHTML = '<div class="editable" onclick="startEdit(this)">' + name + '</div>';
-    // cell2.innerHTML = '<div class="editable" onclick="startEdit(this)">' + email + '</div>';
-    // cell3.innerHTML = "<button onclick='removeName(this)'>Remove</button>";
-
-    nameInput.value = "";
-    guestOfInput.value = "";
-    phoneInput.value = "";
-}
-
-function removeName(button) {
-    var row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
-}
-
-function fetchData() {
-    return fetch('days.json')
-        .then(response => response.json())
-        .then(data => {
-            // Iterate through the data and render each day
-            data.forEach(day => {
-                const dayHtml = renderDay(day);
-                // Append the rendered HTML to the container element
-                const container = document.getElementById('expeditions'); 
-                container.innerHTML += dayHtml;
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-}
-
-//DONT NEED ANYMORE
-// function render(days) {
-//     const element = document.querySelector(".expeditions");
-//     element.innerHTML = days.map(renderDay).join("");
-// }
-
 function main() {
-    // render(days);
-    fetchData();
+    render();
 }
 
 window.addEventListener("DOMContentLoaded", main);
