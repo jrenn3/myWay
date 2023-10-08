373787986001a75cdb285da8a76a09e9e7c56791
Joseph Renner <jrenn3@gmail.com>
2023-10-08 15:40:20 -0500

    adding array for crew and looping for the render

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 
+//CONFIGURATIONS
 /**Initializes my Firebase project from /app which allows use of Firebase services.
   -initializeApp: function is a crucial part of Firebase because it initializes your Firebase project and 
     allows you to use Firebase services within your application.*/
@@ -40,7 +41,8 @@ const db = getDatabase();
 //Creates a reference to data at the root of the database
 const dataRef = ref(db, '/');
 
-/**Function to fetch data from the database
+//DATA DOWNLOAD
+/**Function to fetch data from the database and assign it to the global variable days.
 * -Promise: object represents the eventual completion (or failure) of an asynchronous operation and its
 *   resulting value.
 * -onValue: function is used to attach a callback function that will be triggered whenever the data at a
@@ -54,7 +56,7 @@ function fetchDataFromDatabase() {
     onValue(dataRef, (snapshot) => {
       const data = snapshot.val();
       if (data) {
-        resolve(data);
+        window.days = resolve(data);
       } else {
         reject("No data available");
       }
@@ -64,105 +66,47 @@ function fetchDataFromDatabase() {
   });
 }
 
-//DATA FETCH FROM FIREBASE
-window.days = fetchDataFromDatabase()
-  .then((data) => {
-    // Use the data here in a structured format (e.g., JSON)
-    return data;
-  })
-  .catch((error) => {
-    // Handle errors or the case when no data is available
-    console.error(error);
-  });
-
-//render a single day
-//FIX THE DATE CLASS
-//ADD BACK THE COUNT OF CREW OUT OF TOTAL
-//Consider using a template literal loop or a more dynamic approach to avoid repetitive code for the crew members.
-function renderDay(day) {
-    return `
-    <div class = "day">
-    <p class = "date">${day.date}</p>
-    <p class = "slot">${day.slot}</p>
-    <p class = "event">${day.event}</p>
-    <table id = "crew-${day.date}">
-        <tr class = "header">
-            <td>Crew</td>
-            <td>Guest of</td> 
+//RENDERING
+function renderCrew(crew) {
+  return crew
+    .map((member) => {
+      return `
+        <tr class="person">
+          <td class="name">${member.name}</td>
+          <td class="guestOf">${member.guestOf}</td>
         </tr>
-        ${day.crew.person1.name ? `<tr class = "person1">
-            <td class = "name">${day.crew.person1.name}</td>
-            <td class = "guestOf">${day.crew.person1.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person2.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person2.name}</td>
-            <td class = "guestOf">${day.crew.person2.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person3.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person3.name}</td>
-            <td class = "guestOf">${day.crew.person3.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person4.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person4.name}</td>
-            <td class = "guestOf">${day.crew.person4.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person5.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person5.name}</td>
-            <td class = "guestOf">${day.crew.person5.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person6.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person6.name}</td>
-            <td class = "guestOf">${day.crew.person6.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person7.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person7.name}</td>
-            <td class = "guestOf">${day.crew.person7.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person8.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person8.name}</td>
-            <td class = "guestOf">${day.crew.person8.guestOf}</td> 
-        </tr>` : ""}
-        ${day.crew.person9.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person9.name}</td>
-            <td class = "guestOf">${day.crew.person9.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person10.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person10.name}</td>
-            <td class = "guestOf">${day.crew.person10.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person11.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person11.name}</td>
-            <td class = "guestOf">${day.crew.person11.guestOf}</td>
-        </tr>` : ""}
-        ${day.crew.person12.name ? `<tr class = "person">
-            <td class = "name">${day.crew.person12.name}</td>
-            <td class = "guestOf">${day.crew.person12.guestOf}</td>
-        </tr>` : ""}
+      `;
+    })
+    .join("");
+}
+
+//To-do: ADD BACK THE COUNT OF CREW OUT OF TOTAL
+function renderDay(day) {
+return `
+  <div class="day">
+    <p class="date">${day.date}</p>
+    <p class="slot">${day.slot}</p>
+    <p class="event">${day.event}</p>
+    <table id="crew-${day.date}">
+      <tr class="header">
+        <td>Crew</td>
+        <td>Guest of</td>
+      </tr>
+      ${renderCrew(day.crew)}
     </table>
     <input type="text" id="nameInput-${day.date}" placeholder="New name">
     <input type="text" id="guestOfInput-${day.date}" placeholder="Guest of...">
     <input type="text" id="phoneInput-${day.date}" placeholder="Phone number">
-    <button onclick="addName()">Add Name</button>  
+    <button onclick="addName()">Add Name</button>
   </div>
-  `;
+`;
 }
-
-
-//2)THEN FIGURE OUT SYNTAX FOR UPDATING THE SERVER DATABASE IN REALTIME
-//3)THEN IT IS READY TO SHARE
-//4)ADD AUTHENTICATION
+  
 
 function render(days) {
   const element = document.querySelector("#expeditions");
-  fetchDataFromDatabase()
-    .then((data) => {
-      const dataValues = Object.values(data);//PUT THE OBJECTS IN AN ARRAY SO THE .map CAN WORK
-      element.innerHTML = dataValues.map(renderDay).join("");
-    })
-    .catch((error) => {
-      // Handle errors or the case when no data is available
-      console.error(error);
-    });
+  const dataValues = Object.values(days);//PUT THE OBJECTS IN AN ARRAY SO THE .map CAN WORK
+  element.innerHTML = dataValues.map(renderDay).join("");
 }
 
 function main() {
@@ -171,10 +115,9 @@ function main() {
       render(data);
     })
     .catch((error) => {
-      // Handle errors or the case when no data is available
       console.error(error);
     });
 }
 
 window.addEventListener("DOMContentLoaded", main);
-//MAKE SURE TO npx webpack IN THE TERMINAL AFTER SAVING
+//To-do: Automate npx webpack when saving changes to the code.
