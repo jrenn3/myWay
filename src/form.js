25d0b9bad0bab50d9e91734243da2683283e1d7d
Joseph Renner <jrenn3@gmail.com>
2023-10-08 10:27:38 -0500

    addName logic

--- src/form.js
+++ src/form.js
@@ -1,5 +1,16 @@
 import { fetchDataFromDatabase } from 'index.js';
 
+//DATA FETCH FROM FIREBASE
+window.days = fetchDataFromDatabase()
+  .then((data) => {
+    // Use the data here in a structured format (e.g., JSON)
+    return data;
+  })
+  .catch((error) => {
+    // Handle errors or the case when no data is available
+    console.error(error);
+  });
+
 function addName() {
     var nameInput = document.getElementById("nameInput");
     var guestOfInput = document.getElementById("guestOfInput");
@@ -8,11 +19,11 @@ function addName() {
     var guestOf = guestOfInput.value;
     var phone = phoneInput.value;
   
-    
+
     // Determine the next available person slot (e.g., person9, person10, etc.)
     var nextPersonSlot = null;
     for (var i = 1; i <= 12; i++) {
-      if (!objectToModify["crew"]["person" + i].name) {
+      if (!days["crew"]["person" + i].name) {
         nextPersonSlot = "person" + i;
         break;
       }
@@ -20,7 +31,7 @@ function addName() {
   
     if (nextPersonSlot) {
       // Add the new data to the next available person slot
-      objectToModify["crew"][nextPersonSlot] = {
+      days["crew"][nextPersonSlot] = {
         "name": name,
         "guestOf": guestOf,
         "phone": phone
@@ -33,7 +44,7 @@ function addName() {
   
       // Optionally, you can update the UI to reflect the changes
       // For example, re-render the day's data with the updated crew information
-      renderDay(objectToModify);
+
     } else {
       alert("No available slots for new crew members.");
     }
