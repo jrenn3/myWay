0d7618ee869fdd8f822162787d650f322e6ae929
Joseph Renner <jrenn3@gmail.com>
2024-07-27 20:04:58 -0500

    set up fuction

--- functions/index.js
+++ functions/index.js
@@ -17,6 +17,11 @@ const nodemailer = require('nodemailer');
 const moment = require('moment')
 const cron = require('node-cron');
 
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
 const firebaseRSVPs = {
     apiKey: "AIzaSyAUr7UMMECM0UEFKma786IGJXmSTFk1PGo",
     authDomain: "my-way-rsvps.firebaseapp.com",
@@ -36,7 +41,7 @@ function formatTimestamp(timestamp){
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
-  
+
     return `${year}${month}${day}${hours}${minutes}${seconds}`;
 }
 
@@ -46,70 +51,67 @@ const dbRSVPs = getDatabase(appRSVPs);
 
 const dataRefRSVPs = ref(dbRSVPs, '/');
 
-last24hr = [];
+exports.rsvpChangeTrigger = functions.database
+  .dataRefRSVPs
+  .onWrite(async (change, context) => {
 
-async function get24hrRSVPs(db){
-    const oneDayAgo = formatTimestamp(Date.now() - (24 * 60 * 60 * 1000));
-    
-    try {
-        const snapshot = await get(db);
-        const RSVPs = snapshot.val() || [];
-        last24hr = RSVPs.filter(change => change.timestamp >= oneDayAgo)
-        return last24hr;
-    } catch(error) {
-        console.error("Error retrieving data: " + error.message);
-        return [];
-    };
-}
+        last24hr = [];
 
-function formatEmail(changes) {
-    let emailContent = '<p>My Way crew changes in the last 24hrs</p><ul>';
-    for (const key in changes) {
-      const change = changes[key];
-      let guestSyntax = '';
-      if(change.person.guestOf !== "" && change.person.guestOf !== null && change.person.guestOf !== undefined){
-          guestSyntax = ` (guest of ${change.person.guestOf})`
-      }
-      emailContent += `<li>${change.change_type} ${change.person.name}${guestSyntax} from/to ${change.day} (occured  ${change.timestamp})</li>`;
-    }
-    emailContent += '</ul>';
-    return emailContent;
-  }
+        async function get24hrRSVPs(db){
+            const oneDayAgo = formatTimestamp(Date.now() - (24 * 60 * 60 * 1000));
+            
+            try {
+                const snapshot = await get(db);
+                const RSVPs = snapshot.val() || [];
+                last24hr = RSVPs.filter(change => change.timestamp >= oneDayAgo)
+                return last24hr;
+            } catch(error) {
+                console.error("Error retrieving data: " + error.message);
+                return [];
+            };
+        }
 
-  async function sendEmail(content) {
-    let transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'jrenn3@gmail.com',
-        pass: 'hewx qyni ahdm ngyq'
-      }
+        function formatEmail(changes) {
+            let emailContent = '<p>My Way crew changes in the last 24hrs</p><ul>';
+            for (const key in changes) {
+            const change = changes[key];
+            let guestSyntax = '';
+            if(change.person.guestOf !== "" && change.person.guestOf !== null && change.person.guestOf !== undefined){
+                guestSyntax = ` (guest of ${change.person.guestOf})`
+            }
+            emailContent += `<li>${change.change_type} ${change.person.name}${guestSyntax} from/to ${change.day} (occured  ${change.timestamp})</li>`;
+            }
+            emailContent += '</ul>';
+            return emailContent;
+        }
+
+        async function sendEmail(content) {
+            let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'jrenn3@gmail.com',
+                pass: 'hewx qyni ahdm ngyq'
+            }
+            });
+        
+            let mailOptions = {
+            from: 'jrenn3@gmail.com',
+            to: 'rennerjm@icloud.com',
+            subject: 'My Way RSVPs',
+            html: content
+            };
+        
+            await transporter.sendMail(mailOptions);
+            console.log('Email sent successfully');
+        }
+
+        async function main() {
+            const last24hrChanges = await get24hrRSVPs(dataRefRSVPs);
+            const formattedChanges = formatEmail(last24hrChanges);
+            sendEmail(formattedChanges);
+        };
+
     });
-  
-    let mailOptions = {
-      from: 'jrenn3@gmail.com',
-      to: 'rennerjm@icloud.com',
-      subject: 'My Way RSVPs',
-      html: content
-    };
-  
-    await transporter.sendMail(mailOptions);
-    console.log('Email sent successfully');
-}
-
-async function main() {
-    const last24hrChanges = await get24hrRSVPs(dataRefRSVPs);
-    const formattedChanges = formatEmail(last24hrChanges);
-    sendEmail(formattedChanges);
-}
-
-const task = cron.schedule('00 7,16 * * *', async () => {
-  try {
-    console.log('Cron ran');
-    await main();
-  } catch (error) {
-    console.error('Error in scheduled task:', error);
-  }
-});
 
 console.log('Scheduler is running...');
 
